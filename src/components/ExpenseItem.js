38b131b94398fbffe6ab68091e3d1020775e4622
theintheinsoe1218 <theintheinsoe1218@gmail.com>
2022-01-17 17:38:10 +0630

    Parsing data and props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+function ExpenseItem(props){
+    
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toLocaleDateString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.a}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
